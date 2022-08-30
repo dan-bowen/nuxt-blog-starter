9404eaaafe3ed7a4efd693babc8ebdcdf78c9d1f
Dan Bowen <dan@crucialwebstudio.com>
2022-08-30 13:38:39 -0600

    feat: implement vue-icomoon

---
+++ svc-website/nuxt-app/components/Icon/Icon.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { Icomoon } from "vue-icomoon";
+import iconSet from "./selection.json";
+
+const props = defineProps({
+    name: {
+        type: String,
+        required: true
+    },
+    size: {
+        type: [Number, String],
+        default: '20px'
+    },
+    color: {
+        type: String,
+        default: '#82899a'
+    },
+    style: {
+        type: Object,
+        default: {}
+    },
+    title: {
+        type: String,
+        default: null
+    },
+    class: {
+        type: String,
+        required: false
+    },
+    disableFill: {
+        type: Boolean,
+        default: false
+    },
+    removeInitialStyle: {
+        type: Boolean,
+        default: false
+    }
+})
+</script>
+
+<template>
+    <icomoon :iconSet="iconSet" v-bind="$props" />
+</template>
