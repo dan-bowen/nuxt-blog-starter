f356615434a7db246008c3c528f0cc98deb2e18c
Dan Bowen <dan@crucialwebstudio.com>
2022-09-03 15:52:28 -0600

    chore(deps): add autoanimate addon

--- nuxt-app/formkit.config.ts
+++ nuxt-app/formkit.config.ts
@@ -1,10 +1,14 @@
 import { en } from '@formkit/i18n'
 import { DefaultConfigOptions } from '@formkit/vue'
+import { createAutoAnimatePlugin } from '@formkit/addons'
 import '@formkit/themes/genesis'
 
 const config: DefaultConfigOptions = {
     locales: { en },
     locale: 'en',
+    plugins: [
+        createAutoAnimatePlugin()
+    ]
 }
 
 export default config
