b6c2a93eee42c0cf02a2ddb40aa2466a08eb9419
Dan Bowen <dan@crucialwebstudio.com>
2022-09-03 12:46:23 -0600

    chore(deps): add formkit default theme

--- nuxt-app/formkit.config.ts
+++ nuxt-app/formkit.config.ts
@@ -1,5 +1,6 @@
 import { en } from '@formkit/i18n'
 import { DefaultConfigOptions } from '@formkit/vue'
+import '@formkit/themes/genesis'
 
 const config: DefaultConfigOptions = {
     locales: { en },
