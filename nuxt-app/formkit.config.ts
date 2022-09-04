51bc6f732af05164da56becee8c26b14a1e18d0c
Dan Bowen <dan@crucialwebstudio.com>
2022-09-03 20:49:53 -0600

    chore: remove asterisk plugin

--- nuxt-app/formkit.config.ts
+++ nuxt-app/formkit.config.ts
@@ -3,36 +3,11 @@ import { DefaultConfigOptions } from '@formkit/vue'
 import { createAutoAnimatePlugin } from '@formkit/addons'
 import '@formkit/themes/genesis'
 
-// add an asterisk to required fields
-const isCheckboxAndRadioMultiple = (node) => (node.props.type === 'checkbox' || node.props.type === 'radio') && node.props.options
-function addAsteriskPlugin(node) {
-    node.on('created', () => {
-        const schemaFn = node.props.definition.schema;
-        node.props.definition.schema = (sectionsSchema = {}) => {
-            const isRequired = node.props.parsedRules.some(rule => rule.name === 'required');
-
-            if (isRequired) {
-                if (isCheckboxAndRadioMultiple(node)) {
-                    sectionsSchema.legend = {
-                        children: ['$label', ' *']
-                    }
-                } else {
-                    sectionsSchema.label = {
-                        children: ['$label', ' *']
-                    }
-                }
-            }
-            return schemaFn(sectionsSchema);
-        }
-    })
-}
-
 const config: DefaultConfigOptions = {
     locales: { en },
     locale: 'en',
     plugins: [
-        createAutoAnimatePlugin(),
-        addAsteriskPlugin
+        createAutoAnimatePlugin()
     ]
 }
 
